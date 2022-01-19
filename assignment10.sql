92eab1433b821e5be04969b3bad8ae5a5b68a202
rivka99 <rivkafl99@gmail.com>
2022-01-18 21:15:12 -0700

    added queries

--- assignment10.sql
+++ assignment10.sql
@@ -8,3 +8,26 @@ create table employee(
     DOB date
 );
 
+create table contact(
+    ID int,
+    Address1 varchar(30),
+    Phone int,
+    Email varchar(30)
+
+)
+
+create table payInfo(
+    DepID int,
+    ID int,
+    Bonuses int,
+    401k int
+)
+
+create table Department(
+    ID int,
+    DepID int,
+    Title varchar(30),
+    Subordinates SET,
+
+
+)
